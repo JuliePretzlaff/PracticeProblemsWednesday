fc8ac7501d92a0a77216eeaadbbc95cf6f7d43ae
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-01-22 19:20:20 -0500

    Initial Commit of Wednesday Practice Problems

---
+++ PracticeProblemsWednesday/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemsWednesday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lisa's Muffins
+            int total = 7;
+            Console.WriteLine(total - 2);
+
+            //Mike's Bill
+            Console.WriteLine(total = 13 * 4);
+
+            //Diapers
+            total = 40;
+            Console.WriteLine(total / 8);
+
+            //Trevon
+            total = 41;
+            Console.WriteLine(total - 29);
+
+            //Julia's Run
+            total = 47;
+            Console.WriteLine(total - 30);
+
+            //Envelopes
+            total = 12;
+            Console.WriteLine(total / 3);
+
+            //Norachai's Salad
+            double saladCost = 5.12;
+            Console.WriteLine(saladCost + 27.10);
+
+
+         
+            
+
+
+
+
+
+        }
+    }
+}
